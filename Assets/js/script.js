fe717cbaa358690148ac88bcee705a77fcda36e5
Keerbi Liu <kirby.liu12@gmail.com>
2021-04-26 21:57:28 +1000

    Started on javascript, added background image, timer and blank high score.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,27 @@
-const startBtn = document.getElementById("startBtn")
+/* =========== Button functions =========== */
 
-startBtn.addEventListener("click", startQuiz);
-
+const btnA= document.getElementById("aBtn")
+/* Event listeners for buttons, on click */
+aBtn.addEventListener("click", startQuiz);
+/* Button functions */
 function startQuiz() {
     console.log("Start");
+}
+
+/* =========== Timer functions =========== */
+const timer= document.getElementById("timer")
+let time= 60
+setInterval(countDown, 1000)
+function countDown() {
+    time--
+    timer.innerHTML= time;
+
+    if (time < 30) {
+        timer.setAttribute("style", "color: red;")
+    } 
+
+    if (time === 0) {
+        window.location.href = "index.html"
+    }
+
 }
